b370743c4aacf7bbe4492edbbc4c7998101d4a5c
MikeyCol <88990663+MikeyCol@users.noreply.github.com>
2022-05-19 18:57:42 -0700

    Got cointegration to work

--- arbitrage.py
+++ arbitrage.py
@@ -10,15 +10,17 @@ import sys
 np.set_printoptions(threshold=sys.maxsize)
 coins = pd.DataFrame()
 minDates = []
-for filename in os.scandir('./data/Kaggle'):
+for filename in os.scandir('./data/Historical/Kaggle'):
 	if filename.is_file():
 		frame = pd.read_csv(filename)
 		frame = frame[['Name','Date','Open']]
 		frame['Date'] = pd.to_datetime(frame['Date'])		
 		if coins.empty:
 			coins = frame
-		minDates.append(min(frame['Date']))
-		coins = pd.concat([coins,frame],ignore_index=True)
+			minDates.append(min(frame['Date']))
+		else:
+			minDates.append(min(frame['Date']))
+			coins = pd.concat([coins,frame],ignore_index=True)
 		#coins = pd.merge(coins,frame,on='Date',how='inner')
 		
 		#coins.append(frame)
@@ -34,13 +36,41 @@ X = coins[['date_delta','Open']]
 clusters = OPTICS().fit_predict(X)
 
 coins['clusterLabel'] = clusters
-coins = coins.sort_values(by = 'clusterLabel')
 
-conis = coins.drop(coins[coins.clusterLabel == -1].index, inplace = True)
+coins_noOutliers = coins.copy()
+coins_noOutliers.drop(coins[coins.clusterLabel == -1].index, inplace = True)
 
+cointegrated = []
+clusteredCoins = set()
+for i in range(coins_noOutliers['clusterLabel'].max()+1):
+	names = coins_noOutliers[coins_noOutliers['clusterLabel'] == i]
+	names = names['Name'].unique()
+	if len(names) > 1:
+		clusteredCoins.add(tuple(names))
+	#print(names)
 
 with open('arbirageData.txt','w') as f:
 	f.write(coins.to_string())
+
+for ele in clusteredCoins:
+	print(ele)
+	for i in range(len(ele)-1):
+		for coin2 in ele[i+1:]:
+			co1 = coins[coins['Name'] == ele[i]]
+			co2 = coins[coins['Name'] == coin2]
+
+			#print(len(co1))
+			#print(len(co2))
+
+			coint = stats.coint(co1['Open'], co2['Open'])
+			#print(ele[i])
+			#print(coin2)
+			#print(coint)
+			if coint[1] < 0.05:
+				cointegrated.append((ele[i],coin2))
+print(set(cointegrated))
+
+
 
 
 
